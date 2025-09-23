1e33be7838453023ab044efb2c47c6d00657cab4
truong1504 <125253914+truong1504@users.noreply.github.com>
2025-09-23 23:40:37 +0700

    Add files via upload

---
+++ unit1/app/src/main/java/com/example/unit1/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.unit1   // sửa theo đúng tên project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.example.unit1.ui.theme.Unit1Theme   // sửa theo đúng theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Unit1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyMessage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyMessage() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = """
+                Em tên là Nguyễn Đình Trường
+                Quyết tâm A+ môn Mobile
+                Em xin cam kết không gian lận và tiếp tay cho gian lận,
+                nếu gian lận em sẽ trượt môn
+            """.trimIndent(),
+            fontSize = 20.sp,
+            lineHeight = 28.sp
+        )
+    }
+}
